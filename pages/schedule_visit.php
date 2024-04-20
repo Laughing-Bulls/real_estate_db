69cb2ce4a9365970202ef6b56e3c32e621b84b12
Scott D <90161320+Laughing-Bulls@users.noreply.github.com>
2024-04-20 17:38:37 -0400

    Update schedule_visit.php

--- pages/schedule_visit.php
+++ pages/schedule_visit.php
@@ -8,7 +8,29 @@ $house_id = $_POST['house_id'];
 $visit_date = $_POST['visit_date'];
 
 // Connect to MySQL server
+$connection = mysqli_connect("webhost01.arcs.njit.edu:45320", "asd26", "@Yl&K9Akh0", "asd26");
+
+
+<!-- 
+mysql_connect (<MySQLserver>, <username>, <password>);
+
+mysql_select_db (<dbname>);
+
 $connection = mysqli_connect("localhost", "username", "password", "dbname");
+    $connection = mysqli_connect("webhost01.arcs.njit.edu:45320", "asd26", "@Yl&K9Akh0", "asd26");
+
+    
+    Server: sql1.njit.edu via TCP/IP
+Server type: MySQL
+Server version: 8.0.17 - MySQL Community Server - GPL
+Protocol version: 10
+User: asd26@webhost01.arcs.njit.edu
+Server charset: UTF-8 Unicode (utf8)
+
+mysql_connect (<MySQLserver>, <username>, <password>);
+
+mysql_select_db (<dbname>);
+    -->
 
 // Check connection
 if ($connection === false) {
